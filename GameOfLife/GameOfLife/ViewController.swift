b0339ecb255ae8c6228295a8bfceb0f919a112c1
Robert Mißbach <mail@robert-missbach.com>
2016-08-28 18:01:20 +0200

    Create Xcode project

---
+++ GameOfLife/GameOfLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameOfLife
+//
+//  Created by Robert Mißbach on 28.08.16.
+//  Copyright © 2016 Robert Mißbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
